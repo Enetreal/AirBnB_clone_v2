b4e174e8573f4d6247cdfd77ef38eeacedf0e0b2
Enetreal <wizzarddguru@gmail.com>
2024-04-04 01:08:42 -0700

    done

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,33 +1,17 @@
 #!/usr/bin/env bash
-# Install and Start Nginx with the updated config
-
+# Bash scrip to set up web servers to deploy web_static
 sudo apt-get update
 sudo apt-get -y install nginx
-
-# Create necessary directories if they don't exist
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-
-# Create HTML file
-FILE="<html>
-<head>
-</head>
-<body>
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
     Holberton School
-</body>
-</html>"
-echo "$FILE" > /data/web_static/releases/test/index.html
-
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-SERVER_CONFIG="
-server {
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
-}"
-sudo bash -c "echo '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
-
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i '53i \\tlocation \/hbnb_static {\n\t\t alias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
 sudo service nginx restart
